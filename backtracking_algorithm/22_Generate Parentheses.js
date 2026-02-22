a761e56952f88d5ce08199749fb9c4a7a34890f7
bryan <phuang68@uwo.ca>
2026-02-22 17:57:04 +0800

    feat: 22, 79 in js

---
+++ backtracking_algorithm/22_Generate Parentheses.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    const res = [];
+    const path = [];
+
+    const bt = (open, close) => {
+        if(close === n && open === n) {
+            res.push(path.join(''));
+            return;
+        }
+
+        if(open < n) {
+            path.push('(');
+            bt(open + 1, close);
+            path.pop();
+        }
+
+        if(close < open) {
+            path.push(')');
+            bt(open, close + 1);
+            path.pop();
+        }
+    }
+
+    bt(0, 0);
+
+    return res;
+};
